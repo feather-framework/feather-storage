d44e8b377d9fb63e112f70272553cb8ed193b7b4
Tibor Bödecs <mail.tib@gmail.com>
2023-11-20 19:20:06 +0100

    Fix range (#2)
    - fix range in test suite

--- Tests/XCTFeatherStorageTests/XCTFeatherStorageTests.swift
+++ Tests/XCTFeatherStorageTests/XCTFeatherStorageTests.swift
@@ -10,5 +10,27 @@ import XCTFeatherStorage
 
 final class XCTFeatherStorageTests: XCTestCase {
 
-    func testExample() async throws {}
+    func testClosedRange1() async throws {
+        let value = "test"
+        let range = 1...3
+        let startIndex = value.index(value.startIndex, offsetBy: range.lowerBound)
+        let endIndex = value.index(value.startIndex, offsetBy: range.upperBound)
+        XCTAssertEqual(value[startIndex...endIndex], "est")
+    }
+
+    func testClosedRange2() async throws {
+        let value = "test"
+        let range = 0...2
+        let startIndex = value.index(value.startIndex, offsetBy: range.lowerBound)
+        let endIndex = value.index(value.startIndex, offsetBy: range.upperBound)
+        XCTAssertEqual(value[startIndex...endIndex], "tes")
+    }
+    
+    func testClosedRange3() async throws {
+        let value = "test"
+        let range = 2...3
+        let startIndex = value.index(value.startIndex, offsetBy: range.lowerBound)
+        let endIndex = value.index(value.startIndex, offsetBy: range.upperBound)
+        XCTAssertEqual(value[startIndex...endIndex], "st")
+    }
 }
